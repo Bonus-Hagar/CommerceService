35fcffee30aca4bd26aa5e003bb297b0039ab477
FinnurEB <finnureb@lsretail.com>
2023-04-19 11:00:57 +0000

    PassCreator Updated

---
+++ Common/Core/Loyalty/Domain.DataModel.Loyalty/PassCreator/PassCreatorBody.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSRetail.Omni.Domain.DataModel.Loyalty.PassCreator
+{
+    public class PassCreatorBody
+    {
+        [JsonProperty("userProvidedId")]
+        public string Id { get; set; }
+        //[JsonProperty("barcodeValue")]
+        [JsonIgnore]
+        public string Barcode { get; set; }
+        [JsonProperty("5b361ad1b82342.33286937")]
+        public string CustomerName { get; set; }
+        [JsonProperty("5b361ad1b82592.54970699")]
+        public string CustomerNumber { get; set; }
+    }
+}
